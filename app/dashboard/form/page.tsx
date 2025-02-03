b928ebc8ca954d848c3873c99ca2b2865d1c8d61
Sidharth <sidharthinfernal@gmail.com>
2025-02-03 21:57:12 +0530

    change to prod url

--- app/dashboard/form/page.tsx
+++ app/dashboard/form/page.tsx
@@ -22,13 +22,13 @@ export default function Form(){
                 <span
 
                     className="text-teal-700 font-medium"
-                >{`https://testimonial-vert.vercel.app/${username}/review`}</span>
+                >{`https://endorsement-nine.vercel.app/${username}/review`}</span>
                 <CopyIcon
                     width={15}
                     height={15}
                     className="cursor-pointer "
                     onClick={() => {
-                        const link = `https://testimonial-vert.vercel.app/${username}/review`
+                        const link = `https://endorsement-nine.vercel.app/${username}/review`
                         window.navigator.clipboard.writeText(link);
                         toast.success("The Link has been copied!");
                     }}
